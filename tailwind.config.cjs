504a13a452c9c9a0aa23ee306197f3d2bc61e7df
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-11-04 11:06:44 -0500

    set font for the blog

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,94 +3,95 @@ const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    darkMode: 'class',
-    theme: {
-        extend: {
-            lineHeight: {
-                11: '2.75rem',
-                12: '3rem',
-                13: '3.25rem',
-                14: '3.5rem',
-            },
-            fontFamily: {
-                sans: ['Space Grotesk', ...fontFamily.sans],
-            },
-            colors: {
-                midnight: '#0E0F11',
-                mlight: '#FBFBFB',
-                border: "hsl(var(--border))",
-                input: "hsl(var(--input))",
-                ring: "hsl(var(--ring))",
-                background: "hsl(var(--background))",
-                foreground: "hsl(var(--foreground))",
-                primary: {
-                  DEFAULT: "hsl(var(--primary))",
-                  foreground: "hsl(var(--primary-foreground))",
-                },
-                secondary: {
-                  DEFAULT: "hsl(var(--secondary))",
-                  foreground: "hsl(var(--secondary-foreground))",
-                },
-                destructive: {
-                  DEFAULT: "hsl(var(--destructive))",
-                  foreground: "hsl(var(--destructive-foreground))",
-                },
-                muted: {
-                  DEFAULT: "hsl(var(--muted))",
-                  foreground: "hsl(var(--muted-foreground))",
-                },
-                accent: {
-                  DEFAULT: "hsl(var(--accent))",
-                  foreground: "hsl(var(--accent-foreground))",
-                },
-                popover: {
-                  DEFAULT: "hsl(var(--popover))",
-                  foreground: "hsl(var(--popover-foreground))",
-                },
-                card: {
-                  DEFAULT: "hsl(var(--card))",
-                  foreground: "hsl(var(--card-foreground))",
-                },
-              },
-            typography: ({theme}) => ({
-                DEFAULT: {
-                    css: {
-                        a: {
-                            color: theme('colors.primary.500'),
-                            '&:hover': {
-                                color: `${theme('colors.primary.600')}`,
-                            },
-                            code: {color: theme('colors.primary.400')},
-                        },
-                        'h1,h2': {
-                            fontWeight: '700',
-                            letterSpacing: theme('letterSpacing.tight'),
-                        },
-                        h3: {
-                            fontWeight: '600',
-                        },
-                        code: {
-                            color: theme('colors.indigo.500'),
-                        },
-                    },
-                },
-                invert: {
-                    css: {
-                        a: {
-                            color: theme('colors.primary.500'),
-                            '&:hover': {
-                                color: `${theme('colors.primary.400')}`,
-                            },
-                            code: {color: theme('colors.primary.400')},
-                        },
-                        'h1,h2,h3,h4,h5,h6': {
-                            color: theme('colors.gray.100'),
-                        },
-                    },
-                },
-            }),
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      lineHeight: {
+        11: '2.75rem',
+        12: '3rem',
+        13: '3.25rem',
+        14: '3.5rem'
+      },
+      fontFamily: {
+        sans: ['Geist', 'sans-serif'],
+        heading: ['CalSans', 'sans-serif']
+      },
+      colors: {
+        midnight: '#0E0F11',
+        mlight: '#FBFBFB',
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
         },
-    },
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))'
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
+        }
+      },
+      typography: ({theme}) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: `${theme('colors.primary.600')}`
+              },
+              code: {color: theme('colors.primary.400')}
+            },
+            'h1,h2': {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight')
+            },
+            h3: {
+              fontWeight: '600'
+            },
+            code: {
+              color: theme('colors.indigo.500')
+            }
+          }
+        },
+        invert: {
+          css: {
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: `${theme('colors.primary.400')}`
+              },
+              code: {color: theme('colors.primary.400')}
+            },
+            'h1,h2,h3,h4,h5,h6': {
+              color: theme('colors.gray.100')
+            }
+          }
+        }
+      })
+    }
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
 }

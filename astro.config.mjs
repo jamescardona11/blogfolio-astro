ed73f328d5986a4f38a108f392f7d61e42669c82
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-11-04 00:05:54 -0500

    prettier configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
-import {defineConfig} from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import {defineConfig} from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
-import {SITE_METADATA} from "./src/consts.ts";
+import {SITE_METADATA} from './src/consts.ts'
 
 // https://astro.build/config
 export default defineConfig({
-    site: SITE_METADATA.siteUrl,
-    integrations: [
-        mdx(), 
-        sitemap(), 
-        tailwind({ applyBaseStyles: false })
-    ]
-});
+  site: SITE_METADATA.siteUrl,
+  integrations: [mdx(), sitemap(), tailwind({applyBaseStyles: false})]
+})

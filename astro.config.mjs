e0461d85c75749dd2107fec284a1ff511f9abbfa
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-11-04 23:59:59 -0500

    add shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-
 import tailwind from '@astrojs/tailwind'
-
+import react from '@astrojs/react'
 import { SITE_METADATA } from './src/data/consts.ts'
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_METADATA.siteUrl,
-  integrations: [mdx(), sitemap(), tailwind({ applyBaseStyles: false })]
+
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false
+    }),
+    react({ include: ['**/react/*'] })
+  ]
 })

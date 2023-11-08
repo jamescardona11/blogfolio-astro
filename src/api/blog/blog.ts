0d821c717db8af95925ec06cb16750b0b102c796
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-11-07 21:14:04 -0500

    fetch blog post items

---
+++ src/api/blog/blog.ts
@@ -0,0 +1,51 @@
+import { NOTION_BLOG_DB } from '@/api/server-constants'
+import type { PostItem } from '@/lib/models/post'
+import { notionClient } from '@/lib/notion/notion'
+import { type NBlogPostRow } from '@/lib/notion/notion-models'
+
+const notionDatabaseId = NOTION_BLOG_DB
+
+export async function getLatestBlogPost() {
+  console.log('GET /api/blog')
+
+  try {
+    if (notionDatabaseId == null) {
+      throw new Error('Missing notion secret or DB-ID.')
+    }
+    const query = await notionClient.databases.query({
+      database_id: notionDatabaseId,
+
+      sorts: [
+        {
+          property: 'publishedAt',
+          direction: 'descending'
+        }
+      ]
+    })
+
+    const rows = query.results.map(res => {
+      // @ts-ignore
+      const p = res.properties as NBlogPostRow
+      p.id = res.id
+
+      return p
+    })
+
+    return rows.map(
+      row =>
+        ({
+          id: row.id,
+          title: row.title.title[0].text.content,
+          description: row.description?.rich_text[0]?.text?.content,
+          cover: row.cover?.files[0]?.file?.url,
+          where: row.where.select.name,
+          tag: row.tag?.select?.name,
+          link: row.link?.url,
+          publishedAt: row.publishedAt.date.start
+        }) as PostItem
+    )
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+}
---
+++ src/lib/models/post.ts
@@ -0,0 +1,12 @@
+export interface PostItem {
+  id: string
+  title: string
+  description?: string
+  cover?: string
+  where: string
+  tag?: string
+  link: string
+  series: string
+  order: number
+  publishedAt: string
+}

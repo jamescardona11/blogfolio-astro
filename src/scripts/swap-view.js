e02444e31f4542c3719fe7eaf5b441fbfe26eca5
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-11-29 20:20:57 -0500

    update naming in swap view script

--- src/scripts/swap-view.js
+++ src/scripts/swap-view.js
@@ -3,27 +3,27 @@
 
 // icon-grid-button: This are the selector for the svg grid-icon
 // icon-list-button: This are the selector for the svg list-icon
-const buttonSwapList = document.querySelector('[icon-list-button]')
-const buttonSwapGrid = document.querySelector('[icon-grid-button]')
+const buttonSwapToList = document.querySelector('[icon-list-button]')
+const buttonSwapToGrid = document.querySelector('[icon-grid-button]')
 
 // grid-view: This are the selector for the div that contains the grid
 // list-view: This are the selector for the div that contains the list
 const gridView = document.querySelector('[grid-view]')
 const listView = document.querySelector('[list-view]')
 
-buttonSwapList?.addEventListener('click', changeToGrid)
-buttonSwapGrid?.addEventListener('click', changeToList)
+buttonSwapToList?.addEventListener('click', changeToGrid)
+buttonSwapToGrid?.addEventListener('click', changeToList)
 
 // Set the view for the current path
 // Hide the view and icon for the current view
-readViewForPath()
+init()
 
 function changeToGrid() {
   gridView?.classList.add('hidden')
   listView?.classList.remove('hidden')
 
-  buttonSwapGrid?.classList.remove('hidden')
-  buttonSwapList?.classList.add('hidden')
+  buttonSwapToGrid?.classList.remove('hidden')
+  buttonSwapToList?.classList.add('hidden')
 
   saveViewForPath()
 }
@@ -32,28 +32,28 @@ function changeToList() {
   gridView?.classList.remove('hidden')
   listView?.classList.add('hidden')
 
-  buttonSwapGrid?.classList.add('hidden')
-  buttonSwapList?.classList.remove('hidden')
+  buttonSwapToGrid?.classList.add('hidden')
+  buttonSwapToList?.classList.remove('hidden')
 
   saveViewForPath()
 }
 
 // Default view is grid when the never visited the page
 // to change the default view move the currentView === null to the else statement
-function readViewForPath() {
+function init() {
   const currentPath = window.location.pathname
   const currentView = localStorage.getItem(currentPath)
 
   if (currentView === 'list') {
     // By default we hide the list view and grid icon
-    buttonSwapList?.classList.add('hidden')
-    buttonSwapGrid?.classList.remove('hidden')
+    buttonSwapToList?.classList.add('hidden')
+    buttonSwapToGrid?.classList.remove('hidden')
     gridView?.classList.add('hidden')
     listView?.classList.remove('hidden')
   } else if (currentView == null || currentView === 'grid') {
     // By default we hide the list view and grid icon
-    buttonSwapGrid?.classList.add('hidden')
-    buttonSwapList?.classList.remove('hidden')
+    buttonSwapToGrid?.classList.add('hidden')
+    buttonSwapToList?.classList.remove('hidden')
     listView?.classList.add('hidden')
     gridView?.classList.remove('hidden')
   }

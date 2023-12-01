37543fac1580562b249f60a462a6cc278c7e182a
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-12-01 16:53:43 -0500

    fix intersection

--- src/components/for-pages/blog/table-of-contents/starlight-toc.ts
+++ src/components/for-pages/blog/table-of-contents/starlight-toc.ts
@@ -102,11 +102,9 @@ export class StarlightTOC extends HTMLElement {
   private getRootMargin(): `-${number}px 0% ${number}px` {
     const navBarHeight =
       document.querySelector('header')?.getBoundingClientRect().height || 0
-    // `<summary>` only exists in mobile ToC, so will fall back to 0 in large viewport component.
-    const mobileTocHeight =
-      this.querySelector('summary')?.getBoundingClientRect().height || 0
-    /** Start intersections at nav height + 2rem padding. */
-    const top = navBarHeight + mobileTocHeight + 32
+
+    /** Start intersections at nav height + 2rem padding. + 5rem (20py padding) */
+    const top = navBarHeight + 32 + 80
     /** End intersections 1.5rem later. */
     const bottom = top + 24
     const height = document.documentElement.clientHeight

8e302e170f7cdb69b39ecf7f4e364355d7022591
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-12-01 18:02:44 -0500

    update the scroll to top

--- src/components/for-pages/blog/table-of-contents/toc.ts
+++ src/components/for-pages/blog/table-of-contents/toc.ts
@@ -1,7 +1,6 @@
 const minHeadingLevel = 1
 const maxHeadingLevel = 4
 const topId = '_top'
-const topScrollId = '_top_scroll'
 
 const headingClass =
   'block text-sm transition-all text-zinc-100/50 underline-offset-2 pt-1 hover:text-zinc-100'
@@ -11,7 +10,6 @@ export {
   minHeadingLevel as MIN_HEADING_LEVEL,
   maxHeadingLevel as MAX_HEADING_LEVEL,
   topId as TOP_ID,
-  topScrollId as TOP_SCROLL_ID,
   headingClass as HEADING_CLASS,
   headingHover as HEADING_HOVER
 }

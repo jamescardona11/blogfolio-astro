8d5474c913278576d92d71659f0e842eaefd0539
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-12-09 16:08:16 -0500

    post navigation

--- src/content/post-status.type.ts
+++ src/content/post-status.type.ts
@@ -1,4 +1,4 @@
 import { z } from 'astro/zod'
 
-export type PostStatus = 'draft' | 'partial' | 'published'
-export const postStatusEnum = z.enum(['draft', 'partial', 'published'])
+export type PostStatus = 'draft' | 'in progress' | 'published'
+export const postStatusEnum = z.enum(['draft', 'in progress', 'published'])

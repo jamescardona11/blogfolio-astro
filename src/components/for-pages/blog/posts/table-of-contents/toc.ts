45de5038d7b52c2f84fbfdc101f597634c2b4de1
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-12-09 20:20:39 -0500

    fixing theme issues

--- src/components/for-pages/blog/posts/table-of-contents/toc.ts
+++ src/components/for-pages/blog/posts/table-of-contents/toc.ts
@@ -3,8 +3,9 @@ const maxHeadingLevel = 4
 const topId = '_top'
 
 const headingClass =
-  'block text-sm transition-all text-foreground/50 underline-offset-2 pt-1 hover:text-zinc-100'
-const headingHover = 'hover:text-zinc-100 hover:decoration-zinc-200/50'
+  'block text-sm transition-all text-foreground/50 underline-offset-2 pt-1 hover:text-zinc-700 dark:hover:text-zinc-100'
+const headingHover =
+  'hover:text-zinc-800 hover:decoration-zinc-800 dark:hover:text-zinc-100 dark:hover:decoration-zinc-200/50'
 
 export {
   minHeadingLevel as MIN_HEADING_LEVEL,

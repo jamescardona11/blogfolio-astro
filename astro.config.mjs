17a26429e13b8507fd0768a1a6cddd6b5b8afd48
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-12-21 23:07:16 -0500

    update adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
-import { siteMetadata } from './src/site-metadata'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import { siteMetadata } from './src/site-metadata';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: siteMetadata.siteUrl,
   output: 'hybrid',
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind({
-      applyBaseStyles: false
-    }),
-    react({ include: ['**/react/*'] })
-  ]
-})
+  integrations: [mdx(), sitemap(), tailwind({
+    applyBaseStyles: false
+  }), react({
+    include: ['**/react/*']
+  })],
+  adapter: vercel()
+});
